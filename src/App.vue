472f7a9ad62826a2a253474a4b47899bc9dcb899
Romário Lima <devromariolima@gmail.com>
2025-03-15 18:40:28 -0400

    adds initial Vue project setup with essential files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+
+<style>
+
+*{
+  font-family: 'Courier New', Courier, monospace;
+}
+
+body{
+  margin: 0;
+}
+
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: linear-gradient( #1e3c72, #2a5298, #1c92d2);
+
+}
+
+</style>
